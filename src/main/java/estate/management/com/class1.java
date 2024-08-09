bef530299aab8cade56d19c4ac8de5ee8726ce34
Cagatay Arik <cagatay.arik@gmail.com>
2024-08-09 13:08:28 +0100

    v1 for test2

--- src/main/java/estate/management/com/class1.java
+++
@@ -1,9 +0,0 @@
-package estate.management.com;
-
-public class class1 {
-    int x=1;
-    int y=2;
-    int z=3;
-    //Git remote dan local e cekme
-    //Local repository den remote a gonderme
-}
