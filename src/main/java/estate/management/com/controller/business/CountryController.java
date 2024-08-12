494301352937084e7e937f500f1c5cad5aae92d5
YavuzGnz <gunduz.yavuz@outlook.com>
2024-08-12 23:12:10 +0200

    EP added U01-U03 City, Country, District

---
+++ src/main/java/estate/management/com/controller/business/CountryController.java
@@ -0,0 +1,25 @@
+package estate.management.com.controller.business;
+
+import estate.management.com.domain.administrative.Country;
+import estate.management.com.service.business.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    private final CountryService countryService;
+
+    @Autowired
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+}
---
+++ src/main/java/estate/management/com/service/business/CountryService.java
@@ -0,0 +1,24 @@
+package estate.management.com.service.business;
+
+import estate.management.com.domain.administrative.Country;
+import estate.management.com.repository.business.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    private final CountryRepository countryRepository;
+
+    @Autowired
+    public CountryService(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+}
