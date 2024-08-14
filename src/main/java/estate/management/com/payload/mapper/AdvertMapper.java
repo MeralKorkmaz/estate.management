a835f78a1d3e6db922cbfd4b59f50033b18ab6b4
Meral Korkmaz <korkmazmeralebrar@gmail.com>
2024-08-14 18:27:08 -0500

    mapper,response,request,pageable

---
+++ src/main/java/estate/management/com/payload/mapper/AdvertMapper.java
@@ -0,0 +1,50 @@
+package estate.management.com.payload.mapper;
+
+import estate.management.com.domain.advert.Advert;
+import estate.management.com.payload.request.AdvertRequest;
+import estate.management.com.payload.response.AdvertResponse;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class AdvertMapper {
+
+
+    public AdvertResponse mapAdvertToAdvertResponse(Advert advert){
+        return AdvertResponse.builder()
+                .advertTypeId(advert.getAdvertTypeId())
+                .id(advert.getId())
+                .price(advert.getPrice())
+                .cityId(advert.getCityId())
+                .slug(advert.getSlug())
+                .countryId(advert.getCountryId())
+                .districtId(advert.getDistrictId())
+                .title(advert.getTitle())
+                .categoryId(advert.getCategoryId())
+                .location(advert.getLocation())
+                .description(advert.getDescription())
+                .status(advert.getStatus())
+                .viewCount(advert.getViewCount())
+                .userId(advert.getUserId())
+                .build();
+
+    }
+    public Advert mapAdvertRequestToAdvert(AdvertRequest advertRequest){
+        return Advert.builder()
+                .advertTypeId(advertRequest.getAdvertTypeId())
+                .built_in(advertRequest.getBuilt_in())
+                .price(advertRequest.getPrice())
+                .cityId(advertRequest.getCityId())
+                .slug(advertRequest.getSlug())
+                .countryId(advertRequest.getCountryId())
+                .districtId(advertRequest.getDistrictId())
+                .title(advertRequest.getTitle())
+                .categoryId(advertRequest.getCategoryId())
+                .location(advertRequest.getLocation())
+                .description(advertRequest.getDescription())
+                .status(advertRequest.getStatus())
+                .viewCount(advertRequest.getViewCount())
+                .build();
+    }
+}
---
+++ src/main/java/estate/management/com/payload/response/AdvertResponse.java
@@ -0,0 +1,41 @@
+package estate.management.com.payload.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class AdvertResponse {
+
+
+    private Long id;
+    private String title;
+    private String description;
+    private String slug;
+    private Double price;
+    private int status;
+    private boolean isActive = true;
+    private int viewCount;
+    private String location;
+
+
+
+
+    // FOREIGN KEY FIELDS
+    private int advertTypeId;
+    private int countryId;
+    private int cityId;
+    private int districtId;
+    private int userId;
+    private int categoryId;;
+
+
+
+}
