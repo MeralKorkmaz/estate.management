f98e9e57d9abfb7100c5edd187e4d84424dd6b17
Meral Korkmaz <korkmazmeralebrar@gmail.com>
2024-08-14 23:27:19 -0500

    mapper,request,respose,pageable

--- src/main/java/estate/management/com/controller/business/AdvertController.java
+++ src/main/java/estate/management/com/controller/business/AdvertController.java
@@ -1,35 +1,35 @@
-package estate.management.com.controller.business;
-
-import estate.management.com.payload.request.AdvertRequest;
-import estate.management.com.payload.response.AdvertResponse;
-import estate.management.com.repository.business.AdvertRepository;
-import estate.management.com.service.business.AdvertService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("/adverts")
-public class AdvertController {
-
-
-    public final AdvertService advertService;
-
-    @GetMapping("")
-    public Page<AdvertResponse>getTheAdvertsByPage (
-            @RequestParam(value = "page")  int page,
-            @RequestParam(value = "size") int size,
-            @RequestParam(value = "sort") String sort,
-            @RequestParam(value = "type") String type
-    ){
-
-        return  advertService.getTheAdvertsByPage(page, size, sort, type);
-    }
-
-}
+//package estate.management.com.controller.business;
+//
+//import estate.management.com.payload.request.AdvertRequest;
+//import estate.management.com.payload.response.AdvertResponse;
+//import estate.management.com.repository.business.AdvertRepository;
+//import estate.management.com.service.business.AdvertService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.domain.Page;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//@RequiredArgsConstructor
+//@RequestMapping("/adverts")
+//public class AdvertController {
+//
+//
+//    public final AdvertService advertService;
+//
+//    @GetMapping("")
+//    public Page<AdvertResponse>getTheAdvertsByPage (
+//            @RequestParam(value = "page")  int page,
+//            @RequestParam(value = "size") int size,
+//            @RequestParam(value = "sort") String sort,
+//            @RequestParam(value = "type") String type
+//    ){
+//
+//        return  advertService.getTheAdvertsByPage(page, size, sort, type);
+//    }
+//
+//}
--- src/main/java/estate/management/com/payload/request/AdvertRequest.java
+++ src/main/java/estate/management/com/payload/request/AdvertRequest.java
@@ -1,94 +1,94 @@
-package estate.management.com.payload.request;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
-
-import javax.persistence.Column;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder(toBuilder = true)
-public class AdvertRequest {
-
-    @Column(name = "title", nullable = false, length = 150)
-    @NotNull(message = "Title cannot be null")
-    @Size(min = 5, max = 150, message = "Title must be between {min} and {max} characters")
-    private String title;
-
-    @Column(name = "description", length = 300)
-    @Size(max = 300, message = "Description can contain maximum {max} characters")
-    private String description;
-
-    @Column(name = "slug", nullable = false, length = 200)
-    @NotNull(message = "Slug cannot be null")
-    @Size(min = 5, max = 200,  message = "Slug must be between {min} and {max} characters")
-    private String slug;
-
-
-    @Column(name = "price", nullable = false)
-    @NotNull(message = "Price cannot be null")
-    private Double price;
-
-    @Column(name = "status", nullable = false)
-    @NotNull(message = "Status cannot be null, default 0")
-    private int status = 0;
-
-    // Have an issue here, documentation given type should be boolean but default value shoudl be 0.
-    @Column(name = "built_in", nullable = false)
-    @NotNull(message = "Cannot be null, default false")
-    private Boolean built_in= false;
-
-    //TODO is active means it's published or still selling the item.
-    @Column(name = "is_active", nullable = false)
-    @NotNull(message = "Cannot be null")
-    private boolean isActive = true;
-
-    @Column(name = "view_count", nullable = false)
-    @NotNull(message = "Cannot be null, default 0")
-    private int viewCount = 0;
-
-    // Google embeded code should be stored in this field.
-    @Column(name = "location", nullable = false)
-    @NotNull(message = "Location cannot be null")
-    private String location;
-
-
-    // FOREIGN KEY FIELDS
-
-    @Column(name = "advert_type_id", nullable = false)
-    @NotNull(message = "advertTypeId cannot be null" )
-    private int advertTypeId;
-
-    @Column(name = "country_id", nullable = false)
-    @NotNull(message = "countryId cannot be null" )
-    private int countryId;
-
-    @Column(name = "city_id", nullable = false)
-    @NotNull(message = "cityId cannot be null" )
-    private int cityId;
-
-    @Column(name = "discrict_id", nullable = false)
-    @NotNull(message = "disctrictId cannot be null" )
-    private int districtId;
-
-    @Column(name = "user_id", nullable = false)
-    @NotNull(message = "userId cannot be null" )
-    private int userId;
-
-    @Column(name = "category_id", nullable = false)
-    @NotNull(message = "categoryId cannot be null" )
-    private int categoryId;
-
-
-
-
-
-
-
-
-
-}
+//package estate.management.com.payload.request;
+//
+//import com.fasterxml.jackson.annotation.JsonFormat;
+//import lombok.*;
+//
+//import javax.persistence.Column;
+//import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.Size;
+//import java.time.LocalDateTime;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder(toBuilder = true)
+//public class AdvertRequest {
+//
+//    @Column(name = "title", nullable = false, length = 150)
+//    @NotNull(message = "Title cannot be null")
+//    @Size(min = 5, max = 150, message = "Title must be between {min} and {max} characters")
+//    private String title;
+//
+//    @Column(name = "description", length = 300)
+//    @Size(max = 300, message = "Description can contain maximum {max} characters")
+//    private String description;
+//
+//    @Column(name = "slug", nullable = false, length = 200)
+//    @NotNull(message = "Slug cannot be null")
+//    @Size(min = 5, max = 200,  message = "Slug must be between {min} and {max} characters")
+//    private String slug;
+//
+//
+//    @Column(name = "price", nullable = false)
+//    @NotNull(message = "Price cannot be null")
+//    private Double price;
+//
+//    @Column(name = "status", nullable = false)
+//    @NotNull(message = "Status cannot be null, default 0")
+//    private int status = 0;
+//
+//    // Have an issue here, documentation given type should be boolean but default value shoudl be 0.
+//    @Column(name = "built_in", nullable = false)
+//    @NotNull(message = "Cannot be null, default false")
+//    private Boolean built_in= false;
+//
+//    //TODO is active means it's published or still selling the item.
+//    @Column(name = "is_active", nullable = false)
+//    @NotNull(message = "Cannot be null")
+//    private boolean isActive = true;
+//
+//    @Column(name = "view_count", nullable = false)
+//    @NotNull(message = "Cannot be null, default 0")
+//    private int viewCount = 0;
+//
+//    // Google embeded code should be stored in this field.
+//    @Column(name = "location", nullable = false)
+//    @NotNull(message = "Location cannot be null")
+//    private String location;
+//
+//
+//    // FOREIGN KEY FIELDS
+//
+//    @Column(name = "advert_type_id", nullable = false)
+//    @NotNull(message = "advertTypeId cannot be null" )
+//    private int advertTypeId;
+//
+//    @Column(name = "country_id", nullable = false)
+//    @NotNull(message = "countryId cannot be null" )
+//    private int countryId;
+//
+//    @Column(name = "city_id", nullable = false)
+//    @NotNull(message = "cityId cannot be null" )
+//    private int cityId;
+//
+//    @Column(name = "discrict_id", nullable = false)
+//    @NotNull(message = "disctrictId cannot be null" )
+//    private int districtId;
+//
+//    @Column(name = "user_id", nullable = false)
+//    @NotNull(message = "userId cannot be null" )
+//    private int userId;
+//
+//    @Column(name = "category_id", nullable = false)
+//    @NotNull(message = "categoryId cannot be null" )
+//    private int categoryId;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//}
