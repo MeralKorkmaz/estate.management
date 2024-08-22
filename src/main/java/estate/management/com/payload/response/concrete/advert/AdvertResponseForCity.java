b5741c1cbc089ec825c68e70c4b5492fe924b11c
Meral Korkmaz <korkmazmeralebrar@gmail.com>
2024-08-22 15:29:21 -0500

    the final version

---
+++ src/main/java/estate/management/com/payload/response/concrete/advert/AdvertResponseForCity.java
@@ -0,0 +1,21 @@
+package estate.management.com.payload.response.concrete.advert;
+
+import estate.management.com.domain.administrative.City;
+import estate.management.com.payload.response.concrete.CityResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdvertResponseForCity {
+
+    private  CityResponse[] cityResponses;
+
+
+}
