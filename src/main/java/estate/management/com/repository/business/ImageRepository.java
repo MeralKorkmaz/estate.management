a505d452a12b557bd0a9f129aa8505a8b138f207
Meral Korkmaz <korkmazmeralebrar@gmail.com>
2024-08-25 19:35:22 -0500

    advert part 2

---
+++ src/main/java/estate/management/com/repository/business/ImageRepository.java
@@ -0,0 +1,9 @@
+package estate.management.com.repository.business;
+
+import estate.management.com.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
