63d6e53c5b1d6077dbe48681c4dac9786453b452
kadir <kadirerentuter1991@gmail.com>
2024-09-10 10:31:02 +0300

    contact message dosyasi acildi ve contact entity bu dosyaya atildi

---
+++ src/main/java/estate/management/com/contactmessage/controller/ContactMessageController.java
@@ -0,0 +1,51 @@
+package estate.management.com.contactmessage.controller;
+
+import estate.management.com.contactmessage.dto.ContactRequest;
+import estate.management.com.contactmessage.dto.ContactResponse;
+import estate.management.com.contactmessage.service.ContactMessageService;
+import estate.management.com.payload.response.ResponseMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/contact-messages")
+@RequiredArgsConstructor
+public class ContactMessageController {
+
+    private final ContactMessageService contactMessageService;
+
+    @PostMapping
+    public ResponseMessage<ContactResponse> save (@Valid @RequestBody ContactRequest contactRequest){
+        return contactMessageService.save(contactRequest);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('Admin', 'Manager')")
+    public Page<ContactResponse> getContactMessages (
+            @RequestParam(value = "q", required = false) String q,
+            @RequestParam(value = "status", required = false) Integer status,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "20") int size,
+            @RequestParam(value = "sort", defaultValue = "category_id") String sort,
+            @RequestParam(value = "type", defaultValue = "asc") String type) {
+        return contactMessageService.getContactMessages(q,status,page,size,sort,type);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('Admin', 'Manager')")
+    public ResponseEntity<ContactResponse> getContactMessageById(@PathVariable Long id){
+        return ResponseEntity.ok(contactMessageService.getContactMessageById(id));
+    }
+
+    @DeleteMapping("/id")
+    @PreAuthorize("hasAnyAuthority('Admin', 'Manager')")
+    public ResponseEntity<String> deleteById (@PathVariable Long id){
+        return ResponseEntity.ok(contactMessageService.deleteById(id));
+    }
+
+}
---
+++ src/main/java/estate/management/com/contactmessage/service/ContactMessageService.java
@@ -0,0 +1,66 @@
+package estate.management.com.contactmessage.service;
+
+import estate.management.com.contactmessage.dto.ContactRequest;
+import estate.management.com.contactmessage.dto.ContactResponse;
+import estate.management.com.contactmessage.entity.Contact;
+import estate.management.com.contactmessage.mapper.ContactMapper;
+import estate.management.com.contactmessage.messages.Messages;
+import estate.management.com.contactmessage.repository.ContactMessageRepository;
+import estate.management.com.exception.ResourceNotFoundException;
+import estate.management.com.payload.response.ResponseMessage;
+import estate.management.com.service.helper.PageableHelper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class ContactMessageService {
+
+    private final ContactMessageRepository contactMessageRepository;
+    private final ContactMapper contactMapper;
+    private final PageableHelper pageableHelper;
+
+    public ResponseMessage<ContactResponse> save(ContactRequest contactRequest) {
+
+        Contact contact = contactMapper.requestToContact(contactRequest);
+        Contact savedData = contactMessageRepository.save(contact);
+
+        return ResponseMessage.<ContactResponse>builder()
+                .message("Contact Message Created Successfully")
+                .status(HttpStatus.CREATED)
+                .object(contactMapper.contactToResponse(savedData))
+                .build();
+    }
+
+    public Page<ContactResponse> getContactMessages(String q, Integer status, int page, int size, String sort, String type) {
+
+        Pageable pageable = pageableHelper.getPageable(page, size, sort, type);
+        return contactMessageRepository.getContactMessagesByCriteria(q,status,pageable)
+                .map(contactMapper::contactToResponse);
+
+    }
+
+    public ContactResponse getContactMessageById(Long id) {
+
+        Contact contact = contactMessageRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException(Messages.NOT_FOUND_MESSAGE));
+
+        contact.setStatus(1);
+        contactMessageRepository.save(contact);
+
+        return contactMapper.contactToResponse(contact);
+
+    }
+
+    public String deleteById(Long id) {
+
+        getContactMessageById(id);
+        contactMessageRepository.deleteById(id);
+        return Messages.CONTACT_MESSAGE_DELETED_SUCCESSFULLY;
+
+    }
+}
